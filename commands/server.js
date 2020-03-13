5646af8b7510c60462b7ee384628a132c2edbdb2
Luke Chang <lukechang244@gmail.com>
2020-03-12 19:20:25 -0700

    change process.env into string

--- commands/server.js
+++ commands/server.js
@@ -5,7 +5,7 @@ module.exports = {
   name: '!server',
   description: 'Checks whether or not server is on',
   execute(msg, args) {
-    ms.init(process.env.ADDRESS, 25565, (result) => {
+    ms.init(`${process.env.ADDRESS}`, 25565, (result) => {
       if(ms.online) {
         msg.reply("The server is currently online")
       } else {
